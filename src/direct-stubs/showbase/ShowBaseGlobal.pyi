b8c57e798dc3a95cfbf167fed8cee8da41b6ea15
WMOkiishi <w.muneo.o@gmail.com>
2022-08-20 15:11:34 -0600

    Create stubs for `direct.showbase.ShowBase`
    and `direct.showbase.ShowBaseGlobal`

---
+++ src/direct-stubs/showbase/ShowBaseGlobal.pyi
@@ -0,0 +1,29 @@
+__all__ = []
+
+from panda3d.core import (
+    ClockObject,
+    ConfigPageManager,
+    ConfigVariableManager,
+    NodePath,
+    PandaNode,
+    PandaSystem,
+    PGTop,
+    VirtualFileSystem,
+)
+from panda3d.core import ostream as ostream_type
+from .ShowBase import ShowBase
+
+__dev__: bool
+vfs: VirtualFileSystem
+ostream: ostream_type
+globalClock: ClockObject
+cpMgr: ConfigPageManager
+cvMgr: ConfigVariableManager
+pandaSystem: PandaSystem
+render2d: NodePath[PandaNode]
+aspect2d: NodePath[PGTop]
+hidden: NodePath[PandaNode]
+base: ShowBase  # only exists once an instance of ShowBase is created
+
+def run() -> None: ...
+def inspect(anObject): ...
