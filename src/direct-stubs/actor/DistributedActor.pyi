1a89f8213a621c84c646f996671ea031d66c63a3
WMOkiishi <w.muneo.o@gmail.com>
2022-08-28 13:46:34 -0600

    Complete stubs for `direct.Actor.DistributedActor`

--- src/direct-stubs/actor/DistributedActor.pyi
+++ src/direct-stubs/actor/DistributedActor.pyi
@@ -1,6 +1,21 @@
 __all__ = ['DistributedActor']
 
-from typing import Any
+from typing_extensions import Literal
 
-DistributedActor = Any  # placeholder until the stubs for `direct.distributed` are written
-# class DistributedActor(DistributedNode, Actor): ...
+from ..distributed.DistributedNode import DistributedNode
+from ..distributed.ClientRepository import ClientRepository
+from .Actor import Actor
+
+class DistributedActor(DistributedNode, Actor):
+    DistributedActor_initialized: Literal[1]
+    def __init__(self, cr: ClientRepository) -> None: ...
+    def disable(self) -> None: ...
+    def delete(self) -> None: ...
+    def loop(
+        self,
+        animName: str,
+        restart: bool = True,
+        partName: str | None = None,
+        fromFrame: float | None = None,
+        toFrame: float | None = None,
+    ) -> None: ...
