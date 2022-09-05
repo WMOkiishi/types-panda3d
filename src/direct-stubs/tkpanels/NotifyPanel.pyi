6b8cf96cc4cd769eb4b6fa98da42fdd7a6250102
WMOkiishi <w.muneo.o@gmail.com>
2022-09-04 20:27:19 -0600

    Create stubs for `direct.tkpanels`

---
+++ src/direct-stubs/tkpanels/NotifyPanel.pyi
@@ -0,0 +1,25 @@
+__all__ = ['NotifyPanel']
+
+from tkinter import IntVar, Radiobutton, Toplevel
+from typing import Any
+
+from ..directnotify.DirectNotify import DirectNotify
+from ..directnotify.Notifier import Notifier
+
+Pmw: Any
+
+class NotifyPanel:
+    activeCategory: Notifier | None
+    categoryList: Pmw.ScrolledListBox
+    severity: IntVar
+    fatalSeverity: Radiobutton
+    errorSeverity: Radiobutton
+    warningSeverity: Radiobutton
+    infoSeverity: Radiobutton
+    debugSeverity: Radiobutton
+    spamSeverity: Radiobutton
+    def __init__(self, directNotify: DirectNotify, tl: Toplevel | None = None) -> None: ...
+    def getPandaCategories(self): ...
+    def getPandaCategoriesAsList(self): ...
+    def setActivePandaCategory(self, event=None) -> None: ...
+    def setActiveSeverity(self) -> None: ...
