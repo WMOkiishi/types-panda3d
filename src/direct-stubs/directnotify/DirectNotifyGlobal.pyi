ba78f1bac6e68f25f78661277e4dbb38d054c351
WMOkiishi <w.muneo.o@gmail.com>
2022-09-23 20:25:16 -0600

    Improve stubs for `direct.directnotify`

--- src/direct-stubs/directnotify/DirectNotifyGlobal.pyi
+++ src/direct-stubs/directnotify/DirectNotifyGlobal.pyi
@@ -1,8 +1,7 @@
 __all__ = ['directNotify', 'giveNotify']
 
-from collections.abc import Callable
 from typing import ClassVar
-from typing_extensions import Protocol
+from typing_extensions import Final, Protocol
 
 from .DirectNotify import DirectNotify
 from .Notifier import Notifier
@@ -11,5 +10,5 @@ class _SupportsNotify(Protocol):
     __name__: ClassVar[str]
     notify: ClassVar[Notifier]
 
-directNotify: DirectNotify
-giveNotify: Callable[[type[_SupportsNotify]], None]
+directNotify: Final[DirectNotify]
+def giveNotify(cls: type[_SupportsNotify]) -> None: ...
