b9ace0a92dce96b9e7be483955c8441400a7dab2
WMOkiishi <w.muneo.o@gmail.com>
2022-10-12 13:35:07 -0600

    stubtest: Prevent some scripts from running, allowing all of `direct` to be tested

--- tests/stubtest_direct.py
+++ tests/stubtest_direct.py
@@ -1,6 +1,48 @@
-import subprocess
+from __future__ import annotations
+
 import sys
+from collections.abc import Container, Sequence
+from importlib.abc import Loader, MetaPathFinder
+from importlib.machinery import ModuleSpec
+from importlib.util import spec_from_loader
 from pathlib import Path
+from types import ModuleType
+
+import mypy.stubtest
+
+
+class NonExecutingLoader(Loader):
+    """This `Loader` skips execution of the module's contents."""
+    __slots__ = ()
+
+    def exec_module(self, module: ModuleType) -> None:
+        pass
+
+
+class PathFilter(MetaPathFinder):
+    """This `MetaPathFinder` can be used to prevent
+    certain scripts from running when loaded.
+    """
+    __slots__ = 'modules'
+    modules: Container[str]
+
+    def __init__(self, modules: Container[str]) -> None:
+        self.modules = modules
+
+    def find_spec(
+            self,
+            fullname: str,
+            path: Sequence[str | bytes] | None,
+            target: ModuleType | None = None) -> ModuleSpec | None:
+        if fullname in self.modules:
+            return spec_from_loader(fullname, NonExecutingLoader())
+        else:
+            return None
+
+
+def prevent_running(names: Container[str], *, index: int = 2) -> None:
+    """Prevent scripts with names in `names` from running when imported."""
+    sys.meta_path.insert(index, PathFilter(names))
 
 
 def main() -> int:
@@ -14,32 +56,26 @@ def main() -> int:
         allowlists.append(version_allowlist)
     if platform_allowlist.exists():
         allowlists.append(platform_allowlist)
-    src_dir = Path('..', 'src', 'direct-stubs')
-    modules = (p.stem for p in src_dir.glob('*'))
-    dont_check = {
-        '__init__',
-        'directbase',
-        'directutil',
-        'p3d',
-    }
-    cmd = [
-        sys.executable,
-        '-O',
-        '-m',
-        'mypy.stubtest',
-        *('direct.' + m for m in modules if m not in dont_check),
-    ]
+    args = ['direct']
     for allowlist in allowlists:
-        cmd += [
+        args += [
             '--allowlist',
             str(allowlist),
         ]
-    try:
-        print(' '.join(cmd), file=sys.stderr)
-        subprocess.run(cmd, check=True)
-    except subprocess.CalledProcessError as e:
-        return e.returncode
-    return 0
+    print('Stubtest arguments:', ' '.join(args), file=sys.stderr)
+    # These scripts run on import, interfering with the tests
+    prevent_running({
+        'direct.directbase.ThreeUpStart',
+        'direct.directbase.TestStart',
+        'direct.directutil.MemoryLeakHelpers',
+        'direct.p3d.packp3d',
+        'direct.p3d.pdeploy',
+        'direct.p3d.pmerge',
+        'direct.p3d.ppackage',
+        'direct.p3d.ppatcher',
+    })
+    sys.argv += args
+    return mypy.stubtest.main()
 
 
 if __name__ == '__main__':
